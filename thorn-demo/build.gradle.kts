5f5c23050371e5f8a89fa5ea085d163e8b72fa95
vanqure <sergiuszmecina@icloud.com>
2025-08-26 15:11:03 +0200

    removed demo since we got readme.md examples

--- thorn-demo/build.gradle.kts
+++
@@ -1,9 +0,0 @@
-plugins {
-    `thorn-java`
-    `thorn-repositories`
-}
-
-dependencies {
-    api(project(":thorn-codecs:thorn-codec-jackson"))
-    api(project(":thorn-bridges:thorn-bridge-redis"))
-}
--- thorn-demo/src/dev/vanqure/thorn/demo/User.java
+++
@@ -1,4 +0,0 @@
-package dev.vanqure.thorn.demo;
-
-public record User(String id, String username, long level) {
-}
